16ea98a8970aecc48e4637fc48106f8982a5b9f0
luoluovs <31120160+luoluovs@users.noreply.github.com>
2021-01-22 14:59:14 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+set_time_limit(0);
+//不超时
+error_reporting(E_ALL);
+require "node/html2word/OfficeComMaker.php";
+
+//打印所有的错误
+
+$htmlPath = dirname(__FILE__)."/resources/10.html";
+
+$office = \html2word\OfficeComMaker::getInstance();
+if($office->html2Word($htmlPath)->BaseLineAlignment()->Save("docx")){
+    return "html转word成功";
+}
+return "html转word失败";
+
